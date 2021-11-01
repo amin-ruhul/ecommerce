f2e40191c11c0b38d185cbcd0278028f6d0b9fc7
amin_ruhul <ruhulinfo50@gmail.com>
2021-11-01 11:06:30 +0600

    add router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Home from "./Home";
 import Footer from "./layout/Footer";
 import NavBar from "./layout/NavBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
-      <Home />
+      <div className="container container-fluid">
+        <Route exact path="/" component={Home} />
+      </div>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
