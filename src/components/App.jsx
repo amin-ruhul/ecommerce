22048b61ff4ff9e440a407291ab9299b2dccdd18
amin_ruhul <ruhulinfo50@gmail.com>
2021-11-01 09:20:11 +0600

    add footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./layout/Footer";
 import NavBar from "./layout/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 }
