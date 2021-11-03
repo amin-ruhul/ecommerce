468131b1de4612fefc6487b1eb89ddb074dcfa12
amin_ruhul <ruhulinfo50@gmail.com>
2021-11-03 18:36:23 +0600

    add product details

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function ProductDetails() {
+function ProductDetails({ match }) {
+  console.log(match.params.id);
   return (
     <div className="row f-flex justify-content-around">
       <div className="col-12 col-lg-5 img-fluid" id="product_image">
@@ -11,36 +12,36 @@ function ProductDetails() {
           width="500"
         />
       </div>
-      <div class="col-12 col-lg-5 mt-5">
-        <h3>onn. 32” Class HD (720P) LED Roku Smart TV (100012589)</h3>
+      <div className="col-12 col-lg-5 mt-5">
+        <h3>onn. 32” className HD (720P) LED Roku Smart TV (100012589)</h3>
         <p id="product_id">Product # sklfjdk35fsdf5090</p>
 
         <hr />
 
-        <div class="rating-outer">
-          <div class="rating-inner"></div>
+        <div className="rating-outer">
+          <div className="rating-inner"></div>
         </div>
         <span id="no_of_reviews">(5 Reviews)</span>
 
         <hr />
 
         <p id="product_price">$108.00</p>
-        <div class="stockCounter d-inline">
-          <span class="btn btn-danger minus">-</span>
+        <div className="stockCounter d-inline">
+          <span className="btn btn-danger minus">-</span>
 
           <input
             type="number"
-            class="form-control count d-inline"
+            className="form-control count d-inline"
             value="1"
             readOnly
           />
 
-          <span class="btn btn-primary plus">+</span>
+          <span className="btn btn-primary plus">+</span>
         </div>
         <button
           type="button"
           id="cart_btn"
-          class="btn btn-primary d-inline ml-4"
+          className="btn btn-primary d-inline ml-4"
         >
           Add to Cart
         </button>
@@ -53,7 +54,7 @@ function ProductDetails() {
 
         <hr />
 
-        <h4 class="mt-2">Description:</h4>
+        <h4 className="mt-2">Description:</h4>
         <p>
           Binge on movies and TV episodes, news, sports, music and more! We
           insisted on 720p High Definition for this 32" LED TV, bringing out
@@ -70,64 +71,64 @@ function ProductDetails() {
         <button
           id="review_btn"
           type="button"
-          class="btn btn-primary mt-4"
+          className="btn btn-primary mt-4"
           data-toggle="modal"
           data-target="#ratingModal"
         >
           Submit Your Review
         </button>
 
-        <div class="row mt-2 mb-5">
-          <div class="rating w-50">
+        <div className="row mt-2 mb-5">
+          <div className="rating w-50">
             <div
-              class="modal fade"
+              className="modal fade"
               id="ratingModal"
               tabIndex="-1"
               role="dialog"
               aria-labelledby="ratingModalLabel"
               aria-hidden="true"
             >
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="ratingModalLabel">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="ratingModalLabel">
                       Submit Review
                     </h5>
                     <button
                       type="button"
-                      class="close"
+                      className="close"
                       data-dismiss="modal"
                       aria-label="Close"
                     >
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body">
-                    <ul class="stars">
-                      <li class="star">
-                        <i class="fa fa-star"></i>
+                  <div className="modal-body">
+                    <ul className="stars">
+                      <li className="star">
+                        <i className="fa fa-star"></i>
                       </li>
-                      <li class="star">
-                        <i class="fa fa-star"></i>
+                      <li className="star">
+                        <i className="fa fa-star"></i>
                       </li>
-                      <li class="star">
-                        <i class="fa fa-star"></i>
+                      <li className="star">
+                        <i className="fa fa-star"></i>
                       </li>
-                      <li class="star">
-                        <i class="fa fa-star"></i>
+                      <li className="star">
+                        <i className="fa fa-star"></i>
                       </li>
-                      <li class="star">
-                        <i class="fa fa-star"></i>
+                      <li className="star">
+                        <i className="fa fa-star"></i>
                       </li>
                     </ul>
 
                     <textarea
                       name="review"
                       id="review"
-                      class="form-control mt-3"
+                      className="form-control mt-3"
                     ></textarea>
                     <button
-                      class="btn my-3 float-right review-btn px-4 text-white"
+                      className="btn my-3 float-right review-btn px-4 text-white"
                       data-dismiss="modal"
                       aria-label="Close"
                     >
