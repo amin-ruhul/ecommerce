db6f730e653d7693faeb258e8d732f8da8416f2a
amin_ruhul <ruhulinfo50@gmail.com>
2021-11-16 12:18:36 +0600

    add checkout step component

---
+++ src/components/cart/CheckoutSteps.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function CheckoutSteps({ shiping, confirmOrder, payment }) {
+  return (
+    <div className="checkout-progress d-flex justify-content-center mt-5">
+      {shiping ? (
+        <Link to="/shiping" className="float-right">
+          <div className="triangle2-active"></div>
+          <div className="step active-step">Shiping</div>
+          <div className="triangle-active"></div>
+        </Link>
+      ) : (
+        <Link to="#!" disable>
+          <div className="triangle2-incomplete"></div>
+          <div className="step incomplete">Shiping</div>
+          <div className="triangle-incomplete"></div>
+        </Link>
+      )}
+
+      {confirmOrder ? (
+        <Link to="/confirm" className="float-right">
+          <div className="triangle2-active"></div>
+          <div className="step active-step">Confirm Order</div>
+          <div className="triangle-active"></div>
+        </Link>
+      ) : (
+        <Link to="#!" disable>
+          <div className="triangle2-incomplete"></div>
+          <div className="step incomplete">Confirm Order</div>
+          <div className="triangle-incomplete"></div>
+        </Link>
+      )}
+
+      {payment ? (
+        <Link to="/payment" className="float-right">
+          <div className="triangle2-active"></div>
+          <div className="step active-step">payment</div>
+          <div className="triangle-active"></div>
+        </Link>
+      ) : (
+        <Link to="#!" disable>
+          <div className="triangle2-incomplete"></div>
+          <div className="step incomplete">payment</div>
+          <div className="triangle-incomplete"></div>
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default CheckoutSteps;
--- src/components/cart/Shiping.jsx
+++ src/components/cart/Shiping.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { saveShipingInfo } from "../../actions/cartAction";
 import { countries } from "countries-list";
+import CheckoutSteps from "./CheckoutSteps";
 
 function Shiping({ history }) {
   const { shipingInfo } = useSelector((state) => state.cart);
@@ -29,83 +30,86 @@ function Shiping({ history }) {
   };
 
   return (
-    <div className="row wrapper">
-      <div className="col-10 col-lg-5">
-        <form className="shadow-lg" onSubmit={submitHandel}>
-          <h1 className="mb-4">Shipping Info</h1>
-          <div className="form-group">
-            <label htmlFor="address_field">Address</label>
-            <input
-              type="text"
-              id="address_field"
-              className="form-control"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              required
-            />
-          </div>
+    <>
+      <CheckoutSteps shiping />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow-lg" onSubmit={submitHandel}>
+            <h1 className="mb-4">Shipping Info</h1>
+            <div className="form-group">
+              <label htmlFor="address_field">Address</label>
+              <input
+                type="text"
+                id="address_field"
+                className="form-control"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="city_field">City</label>
-            <input
-              type="text"
-              id="city_field"
-              className="form-control"
-              value={city}
-              required
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="city_field">City</label>
+              <input
+                type="text"
+                id="city_field"
+                className="form-control"
+                value={city}
+                required
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="phone_field">Phone No</label>
-            <input
-              type="phone"
-              id="phone_field"
-              className="form-control"
-              value={phone}
-              required
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="phone_field">Phone No</label>
+              <input
+                type="phone"
+                id="phone_field"
+                className="form-control"
+                value={phone}
+                required
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="postal_code_field">Postal Code</label>
-            <input
-              type="number"
-              id="postal_code_field"
-              className="form-control"
-              value={postalCode}
-              required
-              onChange={(e) => setPostalCode(e.target.value)}
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="postal_code_field">Postal Code</label>
+              <input
+                type="number"
+                id="postal_code_field"
+                className="form-control"
+                value={postalCode}
+                required
+                onChange={(e) => setPostalCode(e.target.value)}
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="country_field">Country</label>
-            <select
-              id="country_field"
-              className="form-control"
-              value={country}
-              required
-              onChange={(e) => setCountry(e.target.value)}
+            <div className="form-group">
+              <label htmlFor="country_field">Country</label>
+              <select
+                id="country_field"
+                className="form-control"
+                value={country}
+                required
+                onChange={(e) => setCountry(e.target.value)}
+              >
+                {countryList.map((country) => (
+                  <option key={country.name}>{country.name}</option>
+                ))}
+              </select>
+            </div>
+
+            <button
+              id="shipping_btn"
+              type="submit"
+              className="btn btn-block py-3"
             >
-              {countryList.map((country) => (
-                <option key={country.name}>{country.name}</option>
-              ))}
-            </select>
-          </div>
-
-          <button
-            id="shipping_btn"
-            type="submit"
-            className="btn btn-block py-3"
-          >
-            CONTINUE
-          </button>
-        </form>
+              CONTINUE
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
